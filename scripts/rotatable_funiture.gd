2033c0667d47d566f97265e9bd4c2bc7b39e1197
bitowl <bitowl@bitowl.de>
2018-01-27 01:23:48 +0100

    Add chair with a base that is movable and a top that is rotatable

--- scripts/rotatable_funiture.gd
+++ scripts/rotatable_funiture.gd
@@ -13,7 +13,9 @@ func dist(var a, var b):
 	return sqrt( (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y))
 
 func _process(delta):
-	var distance = get_node(player).position - position
+	
+	var distance = get_node(player).global_position - global_position
+	print(get_node(player).position)
 	if (distance.length() < MAX_PULL_DISTANCE && Input.is_action_pressed("player_pull")):
 		if !pulling:
 			lastRotation = atan2(distance.x, -distance.y)
