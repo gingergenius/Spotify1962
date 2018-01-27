7a413872e728a801a348a2f7c22298b6b7db035e
Martin Felis <martin@fysx.org>
2018-01-27 19:48:20 +0100

    Caster cleanup

--- RayCast.gd
+++ RayCast.gd
@@ -1,19 +1,16 @@
 extends Node2D
 
 # class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var goal
+var target
 var line
-var source
+var origin
 var debug_line
 var collision
 var space
 var space_state
+var transmission_points = PoolVector2Array()
 
 func updateLinePoints(line, points):
-	print (points.size())
-	
 	for i in range (points.size()):
 		if line.get_point_count() == i:
 			line.add_point(points[i])
@@ -23,10 +20,6 @@ func updateLinePoints(line, points):
 		line.remove_point(line.get_point_count() - 1)
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	goal = get_node("../Goal")
-	source = get_node("../Source")
 	line = get_node("Line")
 	debug_line = get_node("DebugLine")
 	collision = get_node("../Collision")
@@ -34,16 +27,13 @@ func _ready():
 	space_state = Physics2DServer.space_get_direct_state(space)
 	
 #recursively cast rays and append points array until given depth
-func cast_ray(source, goal, points, cur_depth, max_depth):	
+func cast_ray(origin, target, points, cur_depth, max_depth):	
 	if (cur_depth == max_depth):
 		return points
 	else:
 		cur_depth +=1
-		#ray_cast.position = source
-		#ray_cast.cast_to = goal - source
-		#ray_cast.force_raycast_update()
 		
-		var result = space_state.intersect_ray(source, goal)
+		var result = space_state.intersect_ray(origin, target)
 	
 		if result.size() > 0:
 			var collision_point = result.position
@@ -53,34 +43,37 @@ func cast_ray(source, goal, points, cur_depth, max_depth):
 				points.push_back(collision_point)
 			collision.position = collision_point
 			var normal = result.normal
-			var falling = collision_point - source
+			var falling = collision_point - origin
 			
 			if falling.length_squared() < 0.01:
-				points.push_back(goal)
+				points.push_back(target)
 				return points
 				
 			falling = falling.normalized()
 			var reflected = falling.reflect(normal)
-			var new_goal = collision_point + (-reflected) * 2000
-			return cast_ray(collision_point + normal*0.001, new_goal, points, cur_depth, max_depth)
+			var new_target = collision_point + (-reflected) * 2000
+			return cast_ray(collision_point + normal*0.001, new_target, points, cur_depth, max_depth)
 		else:
-			points.push_back(goal)
+			points.push_back(target)
 			return points
 
-func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-	pass
-
 func _physics_process(delta):
+	# set up origin and target
+	origin = self.position
+	target = self.position + Vector2(cos(self.rotation), -sin(self.rotation)) * 2000
+	
+	# check whether we have debug target goal and source	
+	if get_node("../Goal") and get_node("../Source"):
+		origin = to_local(get_node("../Source").position)
+		target = to_local(get_node("../Goal").position)
 	
 	var points = PoolVector2Array()
-	points.push_back(to_local(source.position))
+	points.push_back(origin)
 
 	var debug_points = PoolVector2Array()
-	debug_points.push_back(to_local(source.position))
-	debug_points.push_back(to_local(goal.position))
+	debug_points.push_back(origin)
+	debug_points.push_back(target)
 	updateLinePoints(debug_line, debug_points)
 
-	var final_points = cast_ray(to_local(source.position), to_local(goal.position), points, 0, 5)
-	updateLinePoints(line, final_points)
+	transmission_points = cast_ray(origin, target, points, 0, 5)
+	updateLinePoints(line, transmission_points)
