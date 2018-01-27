318009f564c916f561c80c765be3a29aa5a0855c
Martin Felis <martin@fysx.org>
2018-01-27 13:59:08 +0100

    Added missing files

---
+++ Transmission.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+export var points = PoolVector2Array()
+var transmission_rays = []
+var transmission_ray_scene = load("res://TransmissionRay.tscn")
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	var debugTransmission = get_node("debugTransmission")
+	
+	if debugTransmission and debugTransmission.visible:
+		points = debugTransmission.points
+		print ("Using debug transmission!")
+
+
+func _process(delta):
+	var num_rays = points.size() - 1
+	var rays = get_node("Rays")
+	
+	if points.size() - 1 != rays.get_child_count():
+		var ray_children = rays.get_children()
+		
+		for child in ray_children:
+			print ("Removing child: ", child)
+			rays.remove_child(child)
+		
+		for i in range(points.size() - 1):
+			var origin = points[i]
+			var direction = (points[i + 1] - points[i]).normalized()
+			
+			var ray = transmission_ray_scene.instance()
+			ray.position = origin
+			ray.rotation = direction.angle()
+			print ("Instantiated ray: ", ray)
+		
+			get_node("Rays").call_deferred("add_child", ray)
+
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
