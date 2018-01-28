20c7e24189300c2b4f92ff416324880141c7c0dd
p3732 <p3732@users.noreply.github.com>
2018-01-28 13:16:39 +0100

    win menu uses global script for stage change

---
+++ scripts/win_menu.gd
@@ -0,0 +1,17 @@
+extends Panel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	get_node("Button_continue").connect("pressed",self,"continue")
+	pass
+
+func continue():
+	get_node("/root/global").goto_next_stage()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
